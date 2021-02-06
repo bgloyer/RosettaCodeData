4653b05d7faa452386ad95466492301766a9b2d0
brian gloyer <brian.gloyer@gmail.com>
2021-02-06 12:30:10 -0800

    tree cleanup

--- Task/Tree-from-nesting-levels/C++/tree-from-nesting-levels.cpp
+++ Task/Tree-from-nesting-levels/C++/tree-from-nesting-levels.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// Make a tree that is a vector of either values or other trees
 vector<any> MakeTree(input_iterator auto first, input_iterator auto last, int depth = 1)
 {
     vector<any> tree;
@@ -12,11 +13,13 @@ vector<any> MakeTree(input_iterator auto first, input_iterator auto last, int de
     {
         if(*first == depth)
         {
+            // add a single value
             tree.push_back(*first);
             ++first;
         }
         else // (depth < *b)
         {
+            // add a subtree
             tree.push_back(MakeTree(first, last, depth + 1));
             first = find(first + 1, last, depth); 
         }
@@ -25,21 +28,32 @@ vector<any> MakeTree(input_iterator auto first, input_iterator auto last, int de
     return tree;
 }
 
-
+// Print an input vector or tree
 void PrintTree(input_iterator auto first, input_iterator auto last)
 {
     cout << "[";
     for(auto it = first; it != last; ++it)
     {
         if(it != first) cout << ", ";
-        if(it->type() == typeid(unsigned int))
+        if constexpr (is_integral_v<remove_reference_t<decltype(*first)>>)
         {
-            cout << any_cast<unsigned int>(*it);
+            // for printing the input vector
+            cout << *it;
         }
         else
         {
-            const auto& subTree = any_cast<vector<any>>(*it);
-            PrintTree(subTree.begin(), subTree.end());
+            // for printing the tree
+            if(it->type() == typeid(unsigned int))
+            {
+                // a single value
+                cout << any_cast<unsigned int>(*it);
+            }
+            else
+            {
+                // a subtree
+                const auto& subTree = any_cast<vector<any>>(*it);
+                PrintTree(subTree.begin(), subTree.end());
+            }
         }
     }
     cout << "]";
@@ -47,26 +61,21 @@ void PrintTree(input_iterator auto first, input_iterator auto last)
 
 int main(void) 
 {
-
     auto execises = vector<vector<unsigned int>> {
         {},
-        {1},
-        {1, 1},
-        {2},
-        {2, 2},
-        {3},
         {1, 2, 4},
         {3, 1, 3, 1},
         {1, 2, 3, 1},
         {3, 2, 1, 3},
-        {3, 3, 3, 1, 1, 3, 3, 3},
+        {3, 3, 3, 1, 1, 3, 3, 3}
         };
     
     for(const auto& e : execises)
     {
         auto tree = MakeTree(e.begin(), e.end());
+        PrintTree(e.begin(), e.end());
+        cout << " Nests to:\n"; 
         PrintTree(tree.begin(), tree.end());
-        cout << "\n";
+        cout << "\n\n";
     }
-    
 }
